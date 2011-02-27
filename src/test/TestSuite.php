e4fa849682f34eb4102f71f599603b00cfb477f2
Michael Weibel <mweibel@hsr.ch>
2011-02-27 13:44:17 +0100

    adding docblocks

--- src/test/TestSuite.php
+++ src/test/TestSuite.php
@@ -3,9 +3,18 @@
  * This file is part of the html5wiki package.
  *
  * @copyright 2011 HTML5Wiki Team
+ * @package Html5Wiki
  */
 
+/**
+ * Html5wiki basic testsuite
+ *
+ * @author Michael Weibel <mweibel@hsr.ch>
+ */
 class Html5Wiki_Test_TestSuite extends PHPUnit_Framework_TestSuite {
+	/**
+	 * Static function for creating a suite.
+	 */
 	public static function suite() {
 		return new Html5WikiSuite("HTML5WikiTest");
 	}
