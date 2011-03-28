8ebd7ef9c9aa6e65d630c7adfb909d86c17e05fd
Michael Weibel <mweibel@hsr.ch>
2011-03-28 14:43:15 +0200

    adding prototype for layout; wweha, template decorating works:)

---
+++ library/Html5Wiki/Exception/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the HTML5Wiki Project.
+ *
+ * @author Michael Weibel <mweibel@hsr.ch>
+ * @copyright Html5Wiki 2011
+ * @package Html5Wiki
+ * @subpackage Application
+ */
+
+/**
+ * Description of 404Exception
+ *
+ * @author michael
+ */
+class Html5Wiki_Exception_404 extends Html5Wiki_Exception {
+    //put your code here
+}
+?>
---
+++ templates/layout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Html5Wiki Prototype</title>
+	</head>
+	<body>
+		<?= $this->decoratedContent ?>
+	</body>
+</html>
