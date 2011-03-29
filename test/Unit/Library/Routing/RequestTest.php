a730f8a9dd9c604fd9935cc463829dbdbc23e2db
Michael Weibel <mweibel@hsr.ch>
2011-03-29 11:59:24 +0200

    improve request test

--- test/Unit/Library/Routing/RequestTest.php
+++ test/Unit/Library/Routing/RequestTest.php
@@ -1,4 +1,8 @@
 <?php
+
+require 'library/Html5Wiki/Routing/Interface/Request.php';
+require 'library/Html5Wiki/Routing/Request.php';
+
 /**
  * Request test
  *
@@ -7,80 +11,109 @@
  * @package Test
  * @subpackage Routing
  */
-
-require 'library/Html5Wiki/Routing/Interface/Request.php';
-require 'library/Html5Wiki/Routing/Request.php';
-
 class Test_Unit_Routing_RequestTest extends PHPUnit_Framework_TestCase {
-
+	/**
+	 * Default script name
+	 */
 	const SCRIPT_NAME = '/index.php';
 
-	private $request;
-	private $baseServerVariables;
+	/**
+	 * Request object used for testing
+	 * @var Html5Wii_Routing_Interface_Request
+	 */
+	private $request = null;
 
-    public function setUp() {
+	/**
+	 * Base server variables to be merged with server variables in each testcase
+	 * @var array
+	 */
+	private $baseServerVariables = array();
+
+	/**
+	 * Setup a new request object
+	 */
+	public function setUp() {
 		$this->request = new Html5Wiki_Routing_Request();
 		$this->baseServerVariables = array(
-			'SERVER_NAME'  => 'localhost',
-			'SERVER_PORT'  => '80',
-			'HTTPS'        => false,
+			'SERVER_NAME' => 'localhost',
+			'SERVER_PORT' => '80',
+			'HTTPS' => false,
 			'QUERY_STRING' => '',
 			'HTTP_USER_AGENT' => 'TestCase',
-			'REMOTE_ADDR'     => '127.0.0.1',
-			'REQUEST_METHOD'  => 'GET'
+			'REMOTE_ADDR' => '127.0.0.1',
+			'REQUEST_METHOD' => 'GET'
 		);
 	}
 
+	/**
+	 * Tests a request to http://localhost/
+	 */
 	public function testBasePathEmptyWhenNotUsingSubdirInRootUrl() {
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => self::SCRIPT_NAME,
+			'PHP_SELF' => self::SCRIPT_NAME,
 			'REQUEST_URI' => '/'
 		));
 
 		$this->assertEquals('', $this->request->getBasePath());
 	}
 
+	/**
+	 * Tests a request to http://localhost/html5wiki/
+	 * (Happens when the wiki is not within the root folder of the document root)
+	 */
 	public function testBasePathNotEmptyWhenUsingSubidrInRootUrl() {
 		$path = '/html5wiki/';
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => $path . self::SCRIPT_NAME,
+			'PHP_SELF' => $path . self::SCRIPT_NAME,
 			'REQUEST_URI' => $path
 		));
 
 		$this->assertEquals($path, $this->request->getBasePath());
 	}
 
+	/**
+	 * Tests a request to http://localhost/index.php
+	 */
 	public function testBasePathEmptyWithIndexPhp() {
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => self::SCRIPT_NAME,
+			'PHP_SELF' => self::SCRIPT_NAME,
 			'REQUEST_URI' => self::SCRIPT_NAME
 		));
 
 		$this->assertEquals('', $this->request->getBasePath());
 	}
 
+	/**
+	 * Tests a request to http://localhost/html5wiki/index.php
+	 */
 	public function testBasePathNotEmptyWithIndexPhp() {
 		$path = '/html5wiki/';
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => $path . self::SCRIPT_NAME,
+			'PHP_SELF' => $path . self::SCRIPT_NAME,
 			'REQUEST_URI' => $path . self::SCRIPT_NAME
 		));
 
 		$this->assertEquals($path, $this->request->getBasePath());
 	}
 
+	/**
+	 * Tests the arguments parsing of a request to http://localhost/
+	 */
 	public function testArgumentsEmpty() {
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => self::SCRIPT_NAME,
+			'PHP_SELF' => self::SCRIPT_NAME,
 			'REQUEST_URI' => '/'
 		));
 
 		$this->assertEquals(0, count($this->request->getArguments()));
 	}
 
+	/**
+	 * Tests the arguments parsing of a request to http://localhost/wiki/test
+	 */
 	public function testArgumentsNotEmpty() {
 		$this->setUpRequestVariablesAndParseRequest(array(
-			'PHP_SELF'    => self::SCRIPT_NAME,
+			'PHP_SELF' => self::SCRIPT_NAME,
 			'REQUEST_URI' => '/wiki/test'
 		));
 
@@ -89,14 +122,38 @@ class Test_Unit_Routing_RequestTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('test', $arguments[2]);
 	}
 
+	/**
+	 * Tests the arguments parsing of a request to http://localhost/index.php/wiki/test
+	 */
+	public function testArgumentsCorrectWithIndexPhp() {
+		$this->setUpRequestVariablesAndParseRequest(array(
+			'PHP_SELF' => self::SCRIPT_NAME,
+			'REQUEST_URI' => self::SCRIPT_NAME . '/wiki/test'
+		));
+
+		$arguments = $this->request->getArguments();
+		$this->assertEquals('wiki', $arguments[1]);
+		$this->assertEquals('test', $arguments[2]);
+	}
+
+	/**
+	 * Helper method to fully setup the request
+	 * 
+	 * @param array $serverVariables
+	 */
 	private function setUpRequestVariablesAndParseRequest($serverVariables) {
 		$serverVariables = array_merge($this->baseServerVariables, $serverVariables);
 		$this->request->setServerVariables($serverVariables);
 		$this->request->parse();
 	}
 
+	/**
+	 * Resets the request object
+	 */
 	public function tearDown() {
 		$this->request = null;
 	}
+
 }
+
 ?>
