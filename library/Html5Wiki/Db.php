e5944d7f25f6ce661ce16c15ca1203571efddff1
Nicolas Karrer <nkarrer@hsr.ch>
2011-04-04 14:08:30 +0200

    added missing comments. Fixed db_config template

--- library/Html5Wiki/Db.php
+++ library/Html5Wiki/Db.php
@@ -1,14 +1,42 @@
 <?php
+/**
+ * This file is part of the HTML5Wiki Project.
+ *
+ * @author		Nicolas Karrer <nkarrer@hsr.ch>
+ * @copyright	(c) HTML5Wiki Team 2011
+ * @package		Html5Wiki
+ * @subpackage	Library
+ */
 
-
+// @todo move this to a better place
 include 'db_config.php';
 
+/**
+ * Handles the database
+ * 
+ * @author	Nicolas Karrer <nkarrer@hsr.ch>
+ */
 class Html5Wiki_Db {
 	
+	/**
+	 * Db config
+	 * 
+	 * @var array
+	 */
 	public static $CONFIG = array();
 	
+	/**
+	 * Instance of the database connector
+	 * 
+	 * @var Zend_Db
+	 */
 	protected static $db = null;
 	
+	/**
+	 * Returns an instance of the database
+	 * 
+	 * @return Zend_Db
+	 */
 	public static function db() {
 		if( self::$db === null ) {
 			self::$db = Zend_Db::factory('Pdo_Mysql', self::config());
@@ -17,8 +45,12 @@ class Html5Wiki_Db {
 		return self::$db;
 	}
 	
-	
-	protected static function config() {
+	/**
+	 * Returns the db config array
+	 * 
+	 * @return array
+	 */
+	private static function config() {
 		return self::$CONFIG['database'];
 	}
 }
