fdb316c9ea40a63d5021412c5271a7bf341a7072
Michael Weibel <mweibel@hsr.ch>
2011-04-05 15:51:41 +0200

    fix testsetup for Mysql tests

--- test/Unit/Library/Model/Media/TableTest.php
+++ test/Unit/Library/Model/Media/TableTest.php
@@ -3,6 +3,7 @@
  * Front controller test
  *
  * @author		Nicolas Karrer <nkarrer@hsr.ch>
+ * @author      Michael Weibel <mweibel@hsr.ch>
  * @copyright	(c) HTML5Wiki Team 2011
  * @package		Test
  * @subpackage	Model
@@ -11,7 +12,7 @@
 //require 'library/Zend/Db/Table/Abstract.php';
 //require 'library/Html5Wiki/Model/Media/Table.php';
 
-class Test_Unit_Library_Model_Media_TableTest extends PHPUnit_Framework_TestCase {
+class Test_Unit_Library_Model_Media_TableTest extends Test_Unit_Library_Model_AbstractTest {
 	
 	/**
 	 * @var Html5Wiki_Model_Meida_Table
@@ -23,7 +24,8 @@ class Test_Unit_Library_Model_Media_TableTest extends PHPUnit_Framework_TestCase
 	 * @return void
 	 */
 	public function setUp() {
-		//$this->table = new Html5Wiki_Model_Media_Table();
+		parent::setUp();
+		$this->table = new Html5Wiki_Model_Media_Table(array('db' => $this->db));
 	}
 	
 	/**
