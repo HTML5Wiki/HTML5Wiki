e6cc051d6f3406a060f8367a6c31bfb3edbb535a
Nicolas Karrer <nkarrer@hsr.ch>
2011-04-26 16:24:32 +0200

    added js-trigger for article history

--- templates/wiki/article.php
+++ templates/wiki/article.php
@@ -4,7 +4,7 @@
 		<ol class="capsulebar">
 			<li class="item first active read"><a href="#" class="capsule"><span class="caption">Lesen</span></a></li>
 			<li class="item edit"><a href="#" onclick="Article.loadEditForm( <?php echo $this->wikiPage->id; ?>, <?php echo $this->wikiPage->timestamp ?> );" class="capsule"><span class="caption">Bearbeiten</span></a></li>
-			<li class="item last history"><a href="#" class="capsule"><span class="caption">&Auml;nderungsgeschichte</span></a></li>
+			<li class="item last history"><a href="#" onclick="Article.loadArticleHistory( <?php echo $this->wikiPage->id; ?> );" class="capsule"><span class="caption">&Auml;nderungsgeschichte</span></a></li>
 		</ol>
 	</header>
 	<section>
