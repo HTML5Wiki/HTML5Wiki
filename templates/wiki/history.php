915808ebcd0f1bd717718cd6b6a124f53a659591
manuelalabor <msites@msites.net>
2011-05-04 19:31:33 +0200

    fixed styling for versionhistory

--- templates/wiki/history.php
+++ templates/wiki/history.php
@@ -14,30 +14,26 @@
 
 	<form action="<?php echo $this->urlHelper('wiki', 'diff', $this->wikiPage->permalink) ?>" method="get">
 	<div class="grid_12">
-		<fieldset>
-			<legend class="groupname">Versionen</legend>
-
-			<ol class="versionhistory">
-				<?php foreach($this->versions as $versionGroup => $versions) : ?>
-				<ol class="group <?php echo $versionGroup; ?>">
-					<?php foreach($versions as $version) : ?>
-					<?php $user = $version->getUser(); ?>
-					<li class="version">
-						<input type="radio" name="left" value="<?php echo $version->timestamp; ?>" class="diffselector" />
-						<input type="radio" name="right" value="<?php echo $version->timestamp; ?>" class="diffselector" />
-						<span class="timestamp">
-							<span class="time"><?php echo date('H:i', intval($version->timestamp)); ?></span>,
-							<span class="date"><?php echo date('d.m.Y', intval($version->timestamp)); ?></span>
-						</span>
-						<img src="http://www.gravatar.com/avatar/<?php echo md5($user->email); ?>?s=16&d=mm" class="avatar" />
-						<span class="author"><?php echo $user->name; ?></span>
-						<span class="comment">&quot;<?php echo $version->versionComment; ?>&quot;</span>
-					</li>
-					<?php endforeach; ?>
-				</ol>
+		<ol class="versionhistory">
+			<?php foreach($this->versions as $versionGroup => $versions) : ?>
+			<ol class="timespan <?php echo $versionGroup; ?>">
+				<?php foreach($versions as $version) : ?>
+				<?php $user = $version->getUser(); ?>
+				<li class="version">
+					<input type="radio" name="left" value="<?php echo $version->timestamp; ?>" class="diffselector" />
+					<input type="radio" name="right" value="<?php echo $version->timestamp; ?>" class="diffselector" />
+					<span class="timestamp">
+						<span class="time"><?php echo date('H:i', intval($version->timestamp)); ?></span>,
+						<span class="date"><?php echo date('d.m.Y', intval($version->timestamp)); ?></span>
+					</span>
+					<img src="http://www.gravatar.com/avatar/<?php echo md5($user->email); ?>?s=16&d=mm" class="avatar" />
+					<span class="author"><?php echo $user->name; ?></span>
+					<span class="comment">&quot;<?php echo $version->versionComment; ?>&quot;</span>
+				</li>
 				<?php endforeach; ?>
 			</ol>
-		</fieldset>
+			<?php endforeach; ?>
+		</ol>
 	</div>
 	<div class="clear"></div>
 
