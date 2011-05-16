8d578acd490c5d76c89926d384441fab390f047c
Michael Weibel <mweibel@hsr.ch>
2011-05-16 13:43:14 +0200

    fix sql

--- data/sql/html5wiki_schema.sql
+++ data/sql/html5wiki_schema.sql
@@ -1,61 +1,68 @@
-CREATE SCHEMA IF NOT EXISTS `html5wiki` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `html5wiki` ;
 
 -- -----------------------------------------------------
--- Table `User`
+-- Table `html5wiki`.`User`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `User` (
-  `id`		int(11) NOT NULL AUTO_INCREMENT ,
-  `email`	VARCHAR(255) NOT NULL ,
-  `name`	VARCHAR(255) NULL ,
+DROP TABLE IF EXISTS `html5wiki`.`User` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`User` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `email` VARCHAR(255) NOT NULL ,
+  `name` VARCHAR(255) NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `MediaVersion`
+-- Table `html5wiki`.`MediaVersion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `MediaVersion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT ,
-  `timestamp` int(10) NOT NULL ,
+DROP TABLE IF EXISTS `html5wiki`.`MediaVersion` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`MediaVersion` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `timestamp` INT NOT NULL ,
   `userId` INT NOT NULL ,
   `permalink` VARCHAR(255) NOT NULL ,
   `state` ENUM('PUBLISHED','DRAFT','TRASH') NOT NULL DEFAULT PUBLISHED ,
   `versionComment` VARCHAR(140) NULL ,
   `mediaVersionType` ENUM('ARTICLE','FILE') NOT NULL DEFAULT ARTICLE ,
   PRIMARY KEY (`id`, `timestamp`) ,
-  UNIQUE INDEX `fk_MediaVersion_User` (`userId` ASC) )
+  UNIQUE INDEX `permalink_UNIQUE` (`permalink` ASC) )
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `Tag`
+-- Table `html5wiki`.`Tag`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Tag` (
+DROP TABLE IF EXISTS `html5wiki`.`Tag` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`Tag` (
   `tag` VARCHAR(50) NOT NULL ,
   PRIMARY KEY (`tag`) )
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `ArticleVersion`
+-- Table `html5wiki`.`ArticleVersion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ArticleVersion` (
-  `mediaVersionId` int(11) NOT NULL ,
-  `mediaVersionTimestamp` int(10) NOT NULL ,
+DROP TABLE IF EXISTS `html5wiki`.`ArticleVersion` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`ArticleVersion` (
+  `mediaVersionId` INT NOT NULL ,
+  `mediaVersionTimestamp` INT NOT NULL ,
   `title` VARCHAR(200) NOT NULL ,
   `content` TEXT NOT NULL ,
-  INDEX `fk_ArticleVersion_MediaVersion1` (`mediaVersionId` ASC, `mediaVersionTimestamp` ASC) ,
   PRIMARY KEY (`mediaVersionId`, `mediaVersionTimestamp`) )
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `Mimetype`
+-- Table `html5wiki`.`Mimetype`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Mimetype` (
-  `id` int(11) NOT NULL AUTO_INCREMENT ,
+DROP TABLE IF EXISTS `html5wiki`.`Mimetype` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`Mimetype` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `type` VARCHAR(60) NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `type_UNIQUE` (`type` ASC) )
@@ -63,10 +70,12 @@ ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `License`
+-- Table `html5wiki`.`License`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `License` (
-  `id` int(11) NOT NULL AUTO_INCREMENT ,
+DROP TABLE IF EXISTS `html5wiki`.`License` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`License` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(140) NOT NULL ,
   `description` TEXT NULL ,
   PRIMARY KEY (`id`) ,
@@ -75,33 +84,32 @@ ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `FileVersion`
+-- Table `html5wiki`.`FileVersion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `FileVersion` (
-  `mediaVersionId` int(11) NOT NULL ,
-  `mediaVersionTimestamp` int(10) NOT NULL ,
+DROP TABLE IF EXISTS `html5wiki`.`FileVersion` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`FileVersion` (
+  `mediaVersionId` INT NOT NULL ,
+  `mediaVersionTimestamp` INT NOT NULL ,
   `name` VARCHAR(200) NOT NULL ,
   `filepath` VARCHAR(255) NOT NULL ,
   `description` TEXT NULL ,
   `origin` VARCHAR(140) NULL ,
   `author` VARCHAR(140) NULL ,
-  `mimetypeId` int(11) NOT NULL ,
-  `licenseId` int(11) NOT NULL ,
-  INDEX `fk_FileVersion_MediaVersion1` (`mediaVersionId` ASC, `mediaVersionTimestamp` ASC) ,
-  PRIMARY KEY (`mediaVersionId`, `mediaVersionTimestamp`) ,
-  INDEX `fk_FileVersion_Mimetype1` (`mimetypeId` ASC) ,
-  INDEX `fk_FileVersion_License1` (`licenseId` ASC) )
+  `mimetypeId` INT NOT NULL ,
+  `licenseId` INT NOT NULL ,
+  PRIMARY KEY (`mediaVersionId`, `mediaVersionTimestamp`) )
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `MediaVersionTag`
+-- Table `html5wiki`.`MediaVersionTag`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `MediaVersionTag` (
+DROP TABLE IF EXISTS `html5wiki`.`MediaVersionTag` ;
+
+CREATE  TABLE IF NOT EXISTS `html5wiki`.`MediaVersionTag` (
   `tagTag` VARCHAR(50) NOT NULL ,
-  `mediaVersionId` int(11) NOT NULL ,
-  `mediaVersionTimestamp` int(10) NOT NULL ,
-  PRIMARY KEY (`tagTag`, `mediaVersionId`, `mediaVersionTimestamp`) ,
-  INDEX `fk_Tag_has_MediaVersion_Tag1` (`tagTag` ASC) ,
-  INDEX `fk_MediaVersionTags_MediaVersion1` (`mediaVersionId` ASC, `mediaVersionTimestamp` ASC) )
+  `mediaVersionId` INT NOT NULL ,
+  `mediaVersionTimestamp` INT NOT NULL ,
+  PRIMARY KEY (`tagTag`, `mediaVersionId`, `mediaVersionTimestamp`) )
 ENGINE = MyISAM;
