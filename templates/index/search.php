b1acfbc6942ed138598b27eca99e879706565a6b
manuelalabor <msites@msites.net>
2011-05-17 11:56:08 +0200

    styling of search results

--- templates/index/search.php
+++ templates/index/search.php
@@ -1,16 +1,16 @@
-<article id="content" class="grid_12 content article">
+<section id="content" class="grid_12 content searchresults">
 	<header class="title clearfix">
 		<h1 class="heading"><?php echo $this->translate->_('search') ?></h1>
 	</header>
-	<section>
-		<?php foreach ($this->result as $row): ?>
-		<div class="searchresult">
-			<h1><?php echo str_replace($this->term, '<em>' . $this->term . '</em>', $row->title) ?></h1>
-			<div class="content">
-				<?php echo $this->markDownParser->transform(str_replace($this->term, '<em>' . $this->term . '</em>', $row->content)) ?>
-			</div>
-		</div>
-		<?endforeach; ?>
-	</section>
-	<div class="clear"></div>
+	<ol class="results">
+		<?php foreach($this->results as $result) : ?>
+			<li class="result mediatype-<?php echo strtolower($change->mediaVersionType) ?>">
+				<h2 class="name">
+					<a href="<?php echo $this->urlHelper('wiki', $result->permalink) ?>">
+						<?php echo $result->getCommonName() ?>
+					</a>
+				</h2>
+			</li>
+		<?php endforeach; ?>
+	</ol>
 </article>
