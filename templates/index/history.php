b6fe7a89ea2a7da5c1072f1f1568a47586f26e3e
manuelalabor <msites@msites.net>
2011-05-17 10:12:06 +0200

    enhanced and cleaned markup

--- templates/index/history.php
+++ templates/index/history.php
@@ -9,15 +9,20 @@
 				<?php foreach($changes as $change) : ?>
 				<?php $user = $change->getUser(); ?>
 				<li class="change">
-					<a href="<?php echo $this->urlHelper('wiki', $change->permalink) ?>">
+					<h2 class="name">
+						<a href="<?php echo $this->urlHelper('wiki', $change->permalink) ?>">
+							<?php echo $change->getCommonName() ?>
+						</a>
+					</h2>
+					<p class="meta">
 						<span class="timestamp">
 							<span class="time"><?php echo date('H:i', intval($change->timestamp)); ?></span>,
 							<span class="date"><?php echo date('d.m.Y', intval($change->timestamp)); ?></span>
 						</span>
-					</a>
-					<img src="http://www.gravatar.com/avatar/<?php echo md5($user->email); ?>?s=16&d=mm" class="avatar" />
-					<span class="author"><?php echo $user->name; ?></span>
-					<span class="comment">&quot;<?php echo $change->versionComment; ?>&quot;</span>
+						<img src="http://www.gravatar.com/avatar/<?php echo md5($user->email); ?>?s=16&d=mm" class="avatar" />
+						<span class="author"><?php echo $user->name; ?></span>
+						<span class="comment">&quot;<?php echo $change->versionComment; ?>&quot;</span>
+					</p>
 				</li>
 				<?php endforeach; ?>
 			</ol>
