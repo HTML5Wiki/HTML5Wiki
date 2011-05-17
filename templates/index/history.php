0b492c7d93d7b7d09848a8b9dd99c4a9180365fc
manuelalabor <msites@msites.net>
2011-05-17 09:22:18 +0200

    correct classes for "recent changes"-page

--- templates/index/history.php
+++ templates/index/history.php
@@ -1,18 +1,20 @@
-<article class="grid_12 content article">
+<article class="grid_12 content recentchanges">
 	<header>
 		<h1><?php echo $this->translate->_('recentChanges') ?></h1>
 	</header>
-	<section class="grid_12">
-		<ol class="latest-changes">
+	<section>
+		<ol class="changes">
 			<?php foreach($this->latestChanges as $changeGroup => $changes) : ?>
 			<ol class="timespan <?php echo $changeGroup; ?>">
 				<?php foreach($changes as $change) : ?>
 				<?php $user = $change->getUser(); ?>
-				<li class="version">
-					<span class="timestamp">
-						<span class="time"><?php echo date('H:i', intval($change->timestamp)); ?></span>,
-						<span class="date"><?php echo date('d.m.Y', intval($change->timestamp)); ?></span>
-					</span>
+				<li class="change">
+					<a href="<?php echo $this->urlHelper('wiki', $change->permalink) ?>">
+						<span class="timestamp">
+							<span class="time"><?php echo date('H:i', intval($change->timestamp)); ?></span>,
+							<span class="date"><?php echo date('d.m.Y', intval($change->timestamp)); ?></span>
+						</span>
+					</a>
 					<img src="http://www.gravatar.com/avatar/<?php echo md5($user->email); ?>?s=16&d=mm" class="avatar" />
 					<span class="author"><?php echo $user->name; ?></span>
 					<span class="comment">&quot;<?php echo $change->versionComment; ?>&quot;</span>
