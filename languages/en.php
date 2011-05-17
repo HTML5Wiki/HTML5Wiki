0e6cbeec2add90b066d59393e186a0b8d2d73ad4
manuelalabor <msites@msites.net>
2011-05-17 18:54:02 +0200

    added translation for diff-title

--- languages/en.php
+++ languages/en.php
@@ -1,15 +1,16 @@
 <?php
 
 return array(
-	'read' => 'Read',
-	'edit' => 'Edit',
-	'history' => 'History',
-	'homepage' => 'Homepage',
-	'recentChanges' => 'Recent Changes',
-	'search' => 'Search',
-	'searchResultsFor' => 'Search results for "%1"',
-	'articleContentLegend' => 'Article content',
-	'content' => 'Content',
-	'tags' => 'Tags'
+	'read' => 'Read'
+	,'edit' => 'Edit'
+	,'history' => 'History'
+	,'homepage' => 'Homepage'
+	,'recentChanges' => 'Recent Changes'
+	,'search' => 'Search'
+	,'searchResultsFor' => 'Search results for "%1"'
+	,'articleContentLegend' => 'Article content'
+	,'content' => 'Content'
+	,'tags' => 'Tags'
+	,'compareVersions' => 'Compare Versions'
 );
 ?>
