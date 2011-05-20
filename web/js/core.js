c2866e003fd4ed8c111c38bc7507592ebdb4734a
manuelalabor <msites@msites.net>
2011-05-20 12:50:57 +0200

    added documentation to core.js

--- web/js/core.js
+++ web/js/core.js
@@ -1,5 +1,32 @@
+/**
+ * This is the core of the javascript loading engine of HTML5Wiki.<br/>
+ * It enables the application to add multiple actions to the $(document).ready
+ * event without overriding the handler itself.<br/>
+ * <br/>
+ * Just call appendPageReadyCallback() to add a new action to the
+ * pageready-event.<br/>
+ * <br/>
+ * Please notice: These methods are not encapsulted because for ease of use.
+ *
+ * @author Manuel Alabor
+ */
 var pageReadyCallbacks = new Array();
 
+/**
+ * Adds a new action to the $(document).ready()-event.<br/>
+ * You can pass a method-reference directly to the callback-parameter. In this
+ * case, parameters will be ignored.<br/>
+ * <br/>
+ * Example: <code>appendPageReadyCallback(myCallbackFunction);</code><br/>
+ * <br/>
+ * If you want to add a method-call with specific parameters, pass the full
+ * qualified method name as string and pass an array with your parameters.<br/>
+ * <br/>
+ * Example: <code>appendPageReadyCallback("MyObject.init",["myParam"]);<br/>
+ *
+ * @param callback string or function
+ * @param array with parameters; optional
+ */
 function appendPageReadyCallback(callback, parameters) {
 	var callbackData = '';
 	
@@ -17,6 +44,11 @@ function appendPageReadyCallback(callback, parameters) {
 	pageReadyCallbacks.push(callbackData);
 }
 
+/**
+ * Executes all actions which where added before.
+ *
+ * @see #appendPageReadyCallback(callback,parameters)
+ */
 function runPageReadyCallbacks() {
 	for(var i = 0, l = pageReadyCallbacks.length; i < l; i++) {
 		var callback = pageReadyCallbacks[i];
@@ -44,6 +76,13 @@ function runPageReadyCallbacks() {
 	}
 }
 
+/**
+ * The one and only $(document).ready()-handler.<br/>
+ * It just calls the #runPageReadyCallbacks() method as soon the page/DOM is
+ * fully loaded.
+ *
+ * @see #runPageReadyCallbacks()
+ */
 $(document).ready(function() {
 	runPageReadyCallbacks();
 });
