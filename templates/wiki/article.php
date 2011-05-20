305ee1082532a65a9b8369dff8da9ddee1060596
manuelalabor <msites@msites.net>
2011-05-20 08:41:50 +0200

    moved tags-display from bottom of the page directly up to the bottom of the page-title

--- templates/wiki/article.php
+++ templates/wiki/article.php
@@ -2,24 +2,40 @@
 	$basePath = $this->basePath . '/';
 	$this->javascriptHelper()->appendFile($basePath . 'js/Capsulebar.js');
 	$this->javascriptHelper()->appendScript('Capsulebar.init("' . $this->wikiPage->id . '");');
+	
+	$tagSlug = getTagSlug($this->tags);
+	
+	
+	function getTagSlug($tags) {
+		$tagSlug = '';
+		
+		for($i = 0, $l = sizeof($tags); $i < $l; $i++) {
+			$tag = $tags[$i];
+			$tagSlug .= $tag;
+			if($i < $l-1) $tagSlug .= ', ';
+		}
+		
+		return $tagSlug;
+	}
 ?>
 <article id="content" class="grid_12 content article">
-	<header class="title clearfix">
+	<header class="title clearfix">		
+		<?php if($tagSlug != '') : ?>
+		<div class="heading">
+			<h1><?php echo $this->wikiPage->title ?></h1>
+			<p class="tags">Tags: <?php echo $tagSlug ?></p>
+		</div>
+		<?php else : ?>
 		<h1 class="heading"><?php echo $this->wikiPage->title ?></h1>
+		<?php endif; ?>
+		
 		<?php echo $this->capsulebarHelper($this->wikiPage->permalink); ?>
 	</header>
 	<section>
 		<?php echo $this->markDownParser->transform($this->wikiPage->content) ?>
 	</section>
 	<div class="clear"></div>
-	<section>
-		<h2>Tags</h2>
-		<?php foreach ($this->tags as $tag): ?>
-		<div class="ui-state-default ui-corner-all ui-ptags-tag">
-			<div class="ui-ptags-tag-text"><?php echo $tag ?></div>
-		</div>
-		<?php endforeach; ?>
-	</section>
+	
 	<?php if ($this->ajax === true): ?>
 	<script type="text/javascript">
 		Capsulebar.initializeClickEvents();
