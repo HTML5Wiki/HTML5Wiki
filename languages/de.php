b300e0e74f91933b636752dd5fe3707c3d14b7aa
manuelalabor <msites@msites.net>
2011-05-23 09:10:54 +0200

    added translation for "desiredArticleWithPermalinkNotFound"

--- languages/de.php
+++ languages/de.php
@@ -15,6 +15,7 @@ return array(
 	
 	,'tags' => 'Tags'
 	,'searchForOtherObjectsWithTag' => 'Andere Objekte mit dem Tag \'%s\' suchen...'
+	,'desiredArticleWithPermalinkNotFound' => 'Ihr gew&uuml;nschter Artikel mit der Adresse "%s" konnte leider nicht gefunden werden.<br/>M&ouml;chten Sie einen neuen Artikel mit diesem Titel erstellen?'
 	
 	,'compareVersions' => 'Versionen vergleichen'
 );
