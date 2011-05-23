5194c1224d3de658b6813336d2f128bec52eb874
manuelalabor <msites@msites.net>
2011-05-23 10:00:47 +0200

    added translations for create and dont create fresh article

--- languages/en.php
+++ languages/en.php
@@ -16,6 +16,8 @@ return array(
 	,'tags' => 'Tags'
 	,'searchForOtherObjectsWithTag' => 'Search for other objects containing the tag \'%s\''
 	,'desiredArticleWithPermalinkNotFound' => 'The desired article with the address \'%s\' has not been found.<br/>Would you like to create a new article with this title?'
+	,'create' => 'Create'
+	,'noDontCreate' => 'No, don\'t do anything'
 	
 	,'compareVersions' => 'Compare Versions'
 );
