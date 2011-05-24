530db78b689d4fe8ac9b8daebfdd00db1316b77c
Michael Weibel <mweibel@hsr.ch>
2011-05-24 10:14:41 +0200

    User table: email shouldnt be unique

--- data/sql/html5wiki_schema.sql
+++ data/sql/html5wiki_schema.sql
@@ -9,7 +9,7 @@ CREATE  TABLE IF NOT EXISTS `User` (
   `email` VARCHAR(255) NOT NULL ,
   `name` VARCHAR(255) NULL ,
   PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
+)
 ENGINE = MyISAM;
 
 
