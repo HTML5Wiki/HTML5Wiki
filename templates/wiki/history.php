b37f28b4acbee5ff7e5372cf4c762b5a962d49d8
manuelalabor <msites@msites.net>
2011-05-24 11:11:00 +0200

    - added history.js - added default-selection of versions - if only one version is available, there is now possibility to make a diff anymore

--- templates/wiki/history.php
+++ templates/wiki/history.php
@@ -1,8 +1,27 @@
 <?php
 	$basePath = $this->basePath . '/';
+	$this->javascriptHelper()->appendFile($basePath . 'js/classes/history.js');
 	$this->javascriptHelper()->appendFile($basePath . 'js/classes/article.js');
 	$this->javascriptHelper()->appendFile($basePath . 'js/classes/capsulebar.js');
 	$this->javascriptHelper()->appendScript('appendPageReadyCallback("Capsulebar.init", ["' . $this->article->id . '"]);');
+	$this->javascriptHelper()->appendScript('appendPageReadyCallback(History.init);');
+
+	$numberOfVersions = sizeof($this->versions);
+	$overallVersionCounter = -1;
+	
+	function isVersionChecked($index, $side, $numberOfVersions) {
+		$checked = '';
+		$checkedTemplate = 'checked="checked" ';
+		
+		if($numberOfVersions > 1) {
+			if($side == 'left' && $index == 1) $checked = $checkedTemplate;
+			else if($side == 'right' && $index == 0) $checked = $checkedTemplate;
+		} else {
+			$checked = $checkedTemplate;
+		}
+		
+		return $checked;
+	}
 ?>
 
 <article id="content" class="content history">
@@ -10,19 +29,25 @@
 		<h1 class="heading"><?php echo $this->article->title ?></h1>
 		<?php echo $this->capsulebarHelper($this->article->permalink); ?>
 	</header>
-
 	<div class="clear"></div>
 
+	<?php if($numberOfVersions > 1) : ?>
 	<form action="<?php echo $this->urlHelper('wiki', 'diff', $this->article->permalink) ?>" method="get">
+	<?php endif;?>
 	<div class="grid_12">
 		<ol class="versionhistory">
 			<?php foreach($this->versions as $versionGroup => $versions) : ?>
 			<ol class="timespan <?php echo $versionGroup; ?>">
 				<?php foreach($versions as $version) : ?>
-				<?php $user = $version->getUser(); ?>
+				<?php
+					$overallVersionCounter++;
+					$user = $version->getUser();
+				?>
 				<li class="version">
-					<input type="radio" name="left" value="<?php echo $version->timestamp; ?>" class="diffselector" />
-					<input type="radio" name="right" value="<?php echo $version->timestamp; ?>" class="diffselector" />
+					<?php if($numberOfVersions > 1) : ?>
+					<input type="radio" name="left" value="<?php echo $version->timestamp; ?>" class="diffselector" <?php echo isVersionChecked($overallVersionCounter,'left',$numberOfVersions) ?>/>
+					<input type="radio" name="right" value="<?php echo $version->timestamp; ?>" class="diffselector" <?php echo isVersionChecked($overallVersionCounter,'right',$numberOfVersions) ?>/>
+					<? endif; ?>
 					<span class="timestamp">
 						<span class="time"><?php echo date('H:i', intval($version->timestamp)); ?></span>,
 						<span class="date"><?php echo date('d.m.Y', intval($version->timestamp)); ?></span>
@@ -38,14 +63,17 @@
 	</div>
 	<div class="clear"></div>
 
+	<?php if($numberOfVersions > 1) : ?>
 	<div class="grid_12 bottom-button-bar">
 		<input id="article-history" type="submit" value="<?php echo $this->translate->_('compareVersions') ?>" class="caption large-button diff-button"/>
 	</div>
 	</form>
+	<? endif; ?>
 	
 	<div class="clear"></div>
 	<?php if ($this->ajax === true): ?>
 	<script type="text/javascript">
+		History.init();
 		Capsulebar.initializeClickEvents();
 	</script>
 	<?php endif; ?>
