a5120992b871da210d017296d894482c56c6a7ff
Alexandre Joly <alexandre.joly@mekanics.ch>
2011-05-27 13:05:00 +0200

    edit.php is now translatable

--- languages/de.php
+++ languages/de.php
@@ -14,11 +14,14 @@ return array(
 	,'searchResultsFor' => 'Suchergebnisse f&uuml;r "%s"'
 	,'articleContentLegend' => 'Artikelinhalt'
 	,'content' => 'Inhalt'
+	,'save' => 'Speichern'
+	,'deleteArticle' => 'Diesen Artikel komplett l&ouml;schen'
 	
 	,'title' => 'Titel'
 	,'lastChanged' => 'Zuletzt ge&auml;ndert'
 	
 	,'tags' => 'Tags'
+	,'tag' => 'Tag'
 	,'searchForOtherObjectsWithTag' => 'Andere Objekte mit dem Tag \'%s\' suchen...'
 	,'desiredArticleWithPermalinkNotFound' => 'Der gew&uuml;nschte Artikel mit der Adresse \'%s\' konnte leider nicht gefunden werden.<br/>M&ouml;chten Sie einen neuen Artikel mit diesem Titel erstellen?'
 	,'create' => 'Erstellen'
@@ -38,6 +41,19 @@ return array(
 	,'yes' => 'Ja'
 	,'no'  => 'Nein'
 
+	,'taggingLegend' => 'Tagging'
+	,'taggingText' => 'Ein Artikel kann mit verschiedenen Tags versehen werden, welche sp&auml;ter das Auffinden &uuml;ber die Suche erleichtern k&ouml;nnen.<br/> <em>Tipp:</em> Geben Sie mehrere Tags auf einmal getrennt durch ein Komma ein und bestätigen Sie mit der <em>Eingabetaste</em>.'
+
+	,'authorInformationLegend' => 'Autoreninformation'
+	,'authorName' => 'Ihr Name'
+	,'authorEmail' => 'Ihre E-Mailadresse'
+	,'authorInformationText' => 'Ihr <em>Name</em> sowie Ihre <em>E-Mailadresse</em> werden nur zur internen Identifikation resp. Versionskontrolle abgelegt.<br/>Ihre Daten werden weder weitergegeben noch anderweitig ausgewertet.'
+
+	,'versionCommentLegend' => 'Versionskommentar'
+	,'versionCommentText' => 'Kommentar zur Version <em>(optional)</em>:'
+
 	,'hasIntermediateVersionText' => 'Da war jemand schneller als Sie.<br/>Sie haben nun zwei M&ouml;glichkeiten:<br/> - Speichern Sie Ihren Artikel nochmals und die Version wird &uuml;berschrieben.<br/> - L&ouml;schen Sie Ihre Version, um die \'andere Version\' beizubehalten.'
+	,'overwrite' => '&Uuml;berschreiben'
+	,'rejectChanges' => '&Auml;nderungen verwerfen'
 );
 ?>
