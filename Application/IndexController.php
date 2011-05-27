f972eb9094f86a206bae388e4ce2cb024be8282b
manuelalabor <msites@msites.net>
2011-05-27 15:11:31 +0200

    fixed buildURL call

--- Application/IndexController.php
+++ Application/IndexController.php
@@ -73,7 +73,7 @@ class Application_IndexController extends Html5Wiki_Controller_Abstract {
 				'title' => $result['model']->getCommonName()
 				,'mediaType' => strtolower($result['model']->mediaVersionType)
 				,'matchOrigins' => $result['matchOrigins']
-				,'url'  => $this->router->buildURL('wiki',$result['model']->permalink)
+				,'url'  => $this->router->buildURL(array('wiki',$result['model']->permalink))
 			);
 		}
 		
