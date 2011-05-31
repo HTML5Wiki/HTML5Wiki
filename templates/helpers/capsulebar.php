501dbdbafa8d809f4509761a17d4448955f27a03
manuelalabor <msites@msites.net>
2011-05-31 21:34:55 +0200

    active page gets highlighted again

--- templates/helpers/capsulebar.php
+++ templates/helpers/capsulebar.php
@@ -1,10 +1,18 @@
 <?php
 	$i = -1;  // item counter
+	
+	$activeKey = 'read';
+	foreach($this->items as $key => $item) {
+		if($key === $this->activePage) {
+			$activeKey = $key;
+			break;
+		}
+	}
 ?>
 <ol class="capsulebar">
 	<?php foreach($this->items as $key => $item) : ?>
 	<?php $i++; ?>
-	<li class="item <?php echo $item['class']; ?><?php if($item['active'] === true) echo ' active'; ?><?php if($i == 0) echo ' first' ?><?php if($i == sizeof($this->items)-1) echo ' last' ?>">
+	<li class="item <?php echo $item['class']; ?><?php if($key === $activeKey) echo ' active'; ?><?php if($i == 0) echo ' first' ?><?php if($i == sizeof($this->items)-1) echo ' last' ?>">
 		<a href="<?php echo $item['url']; ?>" class="capsule" accesskey="<?php echo $i+1; ?>" id="capsulebar-<?php echo $key ?>">
 			<span class="caption"><?php echo $item['text']; ?></span>
 		</a>
